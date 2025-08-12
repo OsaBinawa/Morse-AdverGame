b752ac7182cb5ef87fa50e1ccbd09ce85ace14d6
OsaBinawa <OsaBinawa@users.noreply.github.com>
2025-08-12 20:50:08 +0700

    Scoring System

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,11 +3,29 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     PlayerController player;
+    PlatformManager platformManager;
+    [SerializeField] private int score;
+    [SerializeField] private int milestone;
+    [SerializeField] private float speedToAdd;
+    [SerializeField] private float pointsPerSecond = 10f;
+    private float scoreTimer;
     private void Awake()
     {
         player = FindFirstObjectByType<PlayerController>();
+        platformManager = FindFirstObjectByType<PlatformManager>();
     }
-
+    private void Update()
+    {
+        if (Time.timeScale == 0f)
+            return;
+        scoreTimer += pointsPerSecond * Time.deltaTime;
+        if (scoreTimer >= 1f)
+        {
+            int pointsToAdd = Mathf.FloorToInt(scoreTimer);
+            scoreTimer -= pointsToAdd;
+            AddScore(pointsToAdd);
+        }
+    }
     private void OnEnable()
     {
         PlayerController.OnPlayerDied += gameOver;
@@ -16,6 +34,23 @@ public class GameManager : MonoBehaviour
     private void OnDisable()
     {
         PlayerController.OnPlayerDied -= gameOver;
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+
+        if (score % 100 == 0)
+        {
+            milestone = score;
+            OnScoreMilestone(score);
+        }
+    }
+
+    private void OnScoreMilestone(int milestone)
+    {
+        Debug.Log($"Reached {milestone} points!");
+        platformManager.moveSpeed += speedToAdd;
     }
 
     public void gameOver()
