10aedb47f2c978e1149a61dc3739a4078b38b01e
givenariel <hokuace1972@gmail.com>
2025-09-10 10:57:14 +0700

    Outline Shader + Resize Obstacle

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -24,7 +24,7 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject)
+        if (collision.CompareTag("Enemy"))
         {
             Destroy(this.gameObject);
         }
